d4acb41fd9e814058ee88e116f1f8991c465f0ca
Sultan <1skairatuly@gmail.com>
2026-04-17 20:43:52 +0500

    Add mock data for profile

--- profile.swift
+++ profile.swift
@@ -2,3 +2,12 @@ struct ProfileView {
     let title = "Профиль пользователя"
     let name = "Sultan"
 }
+
+
+let profile = ProfileView()
+
+print(profile.title)
+
+print("Имя: \(profile.name)")
+
+print("Город: \(profile.city)")
